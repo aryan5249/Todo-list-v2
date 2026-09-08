f06fae8d3994ada3aef3e664deecbf328f67f872
Aryan Sahu <aryansahuofficial@gmail.com>
2026-09-08 22:53:46 +0530

    add, complete, remove

---
+++ script.js
@@ -0,0 +1,32 @@
+const inputBox = document.getElementById("input-box");
+const ListContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        ListContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    inputBox.focus();  
+}
+ListContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
+
+ListContainer.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        addTask();
+    }
+});
